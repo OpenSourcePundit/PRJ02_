dedb2d38b6d709270f7aae022b14cb15ebbc9031
OpenSourcePundit <shashank00098@gmail.com>
2022-12-22 00:49:54 +0530

    @ files added and complete logic implemented

--- script.js
+++ script.js
@@ -5,20 +5,40 @@ var redBtn = document.querySelector('#redBtn')
 var greenBtn = document.querySelector('#greenBtn')
 var blueBtn = document.querySelector('#blueBtn')
 var output = document.querySelector('#output')
+var color = '';
+var size = 20;
 
 function refresh(){
-    output.innerText = text;
-    
+    output.innerText = text.value;
+    document.getElementById('output').style.color = color;
+    document.getElementById('output').style.fontSize = size+'px';
 }
-function plus(){
-    
-    refresh()
+function plusfn(){
+    size = size+2;
+    refresh();
+}
+function minusfn(){
+    size = size-2;
+    refresh();
+}
+function redfn(){
+    color = 'red';
+    refresh();
+}
+function greenfn(){
+    color = 'green';
+    refresh();
+}
+function bluefn(){
+    color = 'blue';
+    refresh();
 }
 
 
 
-plus.addEventListener('click', plus);
-minus.addEventListener('click',minus);
-redBtn.addEventListener('click', red);
-greenBtn.addEventListener('click',green);
-blueBtn.addEventListener('click',blue);
+
+plus.addEventListener('click', plusfn);
+minus.addEventListener('click',minusfn);
+redBtn.addEventListener('click',redfn);
+greenBtn.addEventListener('click',greenfn);
+blueBtn.addEventListener('click',bluefn);

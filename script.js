be803c353378e3a71f4cb0413e72c7f79baaff15
OpenSourcePundit <shashank00098@gmail.com>
2022-12-22 09:10:24 +0530

    H1H2H3 conversion added

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ var redBtn = document.querySelector('#redBtn')
 var greenBtn = document.querySelector('#greenBtn')
 var blueBtn = document.querySelector('#blueBtn')
 var output = document.querySelector('#output')
+var h1Btn = document.querySelector('#h1Btn')
+var h2Btn = document.querySelector('#h2Btn') 
+var h3Btn = document.querySelector('#h3Btn')
+
 var color = '';
 var size = 20;
 
@@ -33,12 +37,25 @@ function bluefn(){
     color = 'blue';
     refresh();
 }
-
-
+function h1fn(){
+    output.innerText = text.value;
+    output.outerHTML = '<h1  id="output">'+output.innerHTML+'</h1>';
+}
+function h2fn(){
+    output.innerText = text.value;
+    output.outerHTML = '<h2 id="output">'+output.innerHTML+'</h2>';
+}
+function h3fn(){
+    output.innerText = text.value;
+    output.outerHTML = '<h3 id="output">'+output.innerHTML+'</h3>';
+}
 
 
 plus.addEventListener('click', plusfn);
 minus.addEventListener('click',minusfn);
 redBtn.addEventListener('click',redfn);
 greenBtn.addEventListener('click',greenfn);
+h1Btn.addEventListener('click',h1fn);
+h2Btn.addEventListener('click',h2fn);
+h3Btn.addEventListener('click',h3fn);
 blueBtn.addEventListener('click',bluefn);
